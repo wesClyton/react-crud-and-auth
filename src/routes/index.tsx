4af538e961decda3997984891ddcf13b0d8a9707
wesClyton <wes.hinsch@gmail.com>
2023-02-03 15:04:08 -0300

    feature/add tema escuro e troca de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
+import Button from "@mui/material/Button";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}>ToggleTheme</Button>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial"></Navigate>} />
     </Routes>
