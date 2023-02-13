a1a425629914f6127b1383ec55ae8408ed1de2ba
wesClyton <wes.hinsch@gmail.com>
2023-02-13 16:16:12 -0300

    feature/CRUD cidades

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDrawerContext } from '../shared/contexts';
 import {
   Dashboard,
+  DetalheDeCidades,
   DetalheDePessoas,
+  ListagemDeCidades,
   ListagemDePessoas,
 } from '../pages';
 
@@ -17,6 +19,11 @@ export const AppRoutes = () => {
         icon: 'home',
         path: '/pagina-inicial',
         label: 'Página inicial',
+      },
+      {
+        icon: 'location_city',
+        path: '/cidades',
+        label: 'Cidades',
       },
       {
         icon: 'people',
@@ -32,6 +39,9 @@ export const AppRoutes = () => {
 
       <Route path="/pessoas" element={<ListagemDePessoas />} />
       <Route path="/pessoas/detalhe/:id" element={<DetalheDePessoas />} />
+
+      <Route path="/cidades" element={<ListagemDeCidades />} />
+      <Route path="/cidades/detalhe/:id" element={<DetalheDeCidades />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
